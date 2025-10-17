97f2725fbbf3c4ab9a33592ca3c60ae4b15585e1
rozsagyenelaw <rozsagyenelaw@yahoo.com>
2025-10-17 21:01:05 +0200

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -15,7 +15,7 @@ const gmailTransporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: 'rozsagyenelaw1@gmail.com',
-    pass: 'pass: 'nwgg fzkx qguh hsdc''
+    pass: 'nwgg fzkx qguh hsdc'
   }
 });
 
